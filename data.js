7453d8b7f7d432bd174d48abf71c5ff08119452e
Steve Behm <stevtbehm1@gmail.com>
2022-02-24 16:29:48 -0800

    Add a location to your data model to hold the car's x and y coordinates on the page.

---
+++ data.js
@@ -0,0 +1,5 @@
+// var data = {
+//   orientation: 'orient-east',
+//   x: 0,
+//   y: 0
+// };
--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+// query the dom to get the img element
 var $racecarImg = document.querySelector('img');
 
+// create a keydown event listener on the document that
+// will change the img's classname in a certain arrow
+// key was struck
 function turnCar(event) {
   if (event.keyCode === 38) {
     $racecarImg.className = 'orient-north';
